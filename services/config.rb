69d843e229cd068820eca6681cdbed58ffef5657
Nandesh Guru <nandesh@cloudcoreo.com>
2018-05-17 17:24:10 -0700

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -17,7 +17,7 @@ coreo_aws_rule "ec2-ip-address-whitelisted" do
       ranges as var(func: %<ip_permission_filter>s) {
         range as ip_ranges
       }
-      query(func: (%<security_group_filter>s) @cascade {
+      query(func: %<security_group_filter>s) @cascade {
         %<default_predicates>s
         group_id
         relates_to @filter(uid(ranges) AND eq(val(range), "[{:cidr_ip=>\"1.0.0.0/32\"}]")) {{
