a07c125543b55c20e2d0350a3fa56973f4f6aba3
Andrew Armaneous <1041698+armaneous@users.noreply.github.com>
2018-05-23 18:26:31 -0700

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -285,25 +285,6 @@ coreo_aws_rule "s3-allusers-write-acp" do
                           })
 end
 
-coreo_aws_rule "s3-authenticatedusers-access" do
-  action :define
-  service :s3
-  link "http://kb.cloudcoreo.com/mydoc_s3-world-open-policy-list.html"
-  display_name "Any Authenticated User has access"
-  description "Bucket policy gives any authenticated user some sort of access to this s3 bucket"
-  category "Security"
-  suggested_action "Remove or modify the bucket policy that enables any authenticated user access."
-  level "High"
-  meta_nist_171_id "3.1.3"
-  objectives     ["buckets", "bucket_policy"]
-  call_modifiers [{}, {:bucket => "buckets.name"}]
-  audit_objects ["", "object.policy"]
-  formulas      ["", "jmespath.Statement[?Effect == 'Allow' && !Condition].Principal"]
-  operators     ["", "=~"]
-  raise_when    ["", /"AWS":\s*"\*"/]
-  id_map "modifiers.bucket"
-end
-
 coreo_aws_rule "s3-authenticatedusers-write" do
   action :define
   service :s3
