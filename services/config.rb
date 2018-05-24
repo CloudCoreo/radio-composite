6c46f9a9d32770f4fad7ebb439d922ac2c01f410
Nandesh Guru <nandesh@cloudcoreo.com>
2018-05-24 14:54:44 -0700

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -6,10 +6,11 @@ coreo_aws_rule "administrative-policy-exposed-by-connected-ssh-credential" do
   description "A publicly routable and addressable ec2 instance has the same ssh key as an instance with an administrative policy."
   category "Security"
   suggested_action "Generate distinct ssh keys per subnet or ec2 instance role."
-  level "Medium"
-  objectives [""]
-  audit_objects [""]
-  operators [""]
+  level "High"
+  objectives ["instances"]
+  audit_objects ["object.reservations.instances.instance_id"]
+  operators ["=~"]
+  raise_when [//]
   meta_rule_query <<~QUERY
 {
   gateways as var(func: %<internet_gateway_filter>s)  @cascade {
